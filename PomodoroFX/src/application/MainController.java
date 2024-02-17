c54d45c9ac4fb3295ac41f1b3dcff08935a001e5
Miguel-Tirado <miguel..e.tirado11@gmail.com>
2024-02-16 16:43:48 -0800

    Get Base functional version of count down timer

--- PomodoroFX/src/application/MainController.java
+++ PomodoroFX/src/application/MainController.java
@@ -1,5 +1,51 @@
 package application;
 
-public class MainController {
+import java.net.URL;
+import java.time.temporal.ChronoUnit;
+import java.util.ResourceBundle;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+public class MainController implements Initializable{
+	
+	@FXML
+	private Label myLabel = new Label(); // Create label to display time left
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		// Create duration property for time remaining
+		ObjectProperty<java.time.Duration> remainingDuration = new SimpleObjectProperty<>(java.time.Duration.ofSeconds(120));
+		
+		// Binding with media time format (mm:ss)
+		myLabel.textProperty().bind(Bindings.createStringBinding(() -> 
+				String.format("%02d:%02d",
+						remainingDuration.get().toMinutesPart(),
+						remainingDuration.get().toSecondsPart()),
+				remainingDuration));
+		
+		// Create time line to lower remaining duration every second:
+		Timeline countDownTimeLine = new Timeline(new KeyFrame(Duration.seconds(1), (ActionEvent event) -> 
+			remainingDuration.setValue(remainingDuration.get().minus(1, ChronoUnit.SECONDS))));
+		
+		// Set number of cycles (remaining duration in Seconds)
+		countDownTimeLine.setCycleCount((int) remainingDuration.get().getSeconds());
+		
+		// Show alert when time is up (optional?)
+		countDownTimeLine.setOnFinished(event -> new Alert(Alert.AlertType.INFORMATION).show());
+		
+		// Start the time line
+		countDownTimeLine.play();
+
+	}
 }
