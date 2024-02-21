15f8702cb29574de67093047c7965953007100f7
Miguel-Tirado <miguel..e.tirado11@gmail.com>
2024-02-20 16:27:06 -0800

    Added Play, Pause, and Reset Functionality. Found bug when the timer is 0 user is able to start the timer and have negative time count down. Need to find solution.

--- PomodoroFX/src/application/MainController.java
+++ PomodoroFX/src/application/MainController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.time.temporal.ChronoUnit;
 import java.util.ResourceBundle;
 
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.beans.binding.Bindings;
@@ -13,6 +14,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
 
@@ -20,12 +22,19 @@ public class MainController implements Initializable{
 	
 	@FXML
 	private Label myLabel = new Label(); // Create label to display time left
+	@FXML
+	private Button myPlayButton, myPauseButton, myResetButton;
+	
+	private ObjectProperty<java.time.Duration> remainingDuration = new SimpleObjectProperty<>(java.time.Duration.ofSeconds(120));
+	
+	private Timeline countDownTimeLine = new Timeline(new KeyFrame(Duration.seconds(1), (ActionEvent event) -> 
+	remainingDuration.setValue(remainingDuration.get().minus(1, ChronoUnit.SECONDS))));
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
 		// Create duration property for time remaining
-		ObjectProperty<java.time.Duration> remainingDuration = new SimpleObjectProperty<>(java.time.Duration.ofSeconds(120));
+		//ObjectProperty<java.time.Duration> remainingDuration = new SimpleObjectProperty<>(java.time.Duration.ofSeconds(120));
 		
 		// Binding with media time format (mm:ss)
 		myLabel.textProperty().bind(Bindings.createStringBinding(() -> 
@@ -35,8 +44,8 @@ public class MainController implements Initializable{
 				remainingDuration));
 		
 		// Create time line to lower remaining duration every second:
-		Timeline countDownTimeLine = new Timeline(new KeyFrame(Duration.seconds(1), (ActionEvent event) -> 
-			remainingDuration.setValue(remainingDuration.get().minus(1, ChronoUnit.SECONDS))));
+		/*Timeline countDownTimeLine = new Timeline(new KeyFrame(Duration.seconds(1), (ActionEvent event) -> 
+			remainingDuration.setValue(remainingDuration.get().minus(1, ChronoUnit.SECONDS)))); */
 		
 		// Set number of cycles (remaining duration in Seconds)
 		countDownTimeLine.setCycleCount((int) remainingDuration.get().getSeconds());
@@ -45,7 +54,20 @@ public class MainController implements Initializable{
 		countDownTimeLine.setOnFinished(event -> new Alert(Alert.AlertType.INFORMATION).show());
 		
 		// Start the time line
-		countDownTimeLine.play();
+		//countDownTimeLine.play();
 
+	}
+	
+	public void playTimer() {
+		countDownTimeLine.play();
+	}
+	
+	public void pauseTimer() {
+		countDownTimeLine.pause();
+	}
+	
+	public void resetTimer() {
+		countDownTimeLine.stop();
+		remainingDuration.set(java.time.Duration.ofSeconds(120));
 	}
 }
